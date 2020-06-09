ee9795fb7091c1a511c4e56606d07951dfed70b3
huni43 <huning43@naver.com>
2020-06-09 21:12:54 +0900

    gameTest

--- src/test/java/clock/GameTest.java
+++ src/test/java/clock/GameTest.java
@@ -1,5 +1,30 @@
+package clock;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.lang.reflect.Field;
 
 class GameTest {
 
+    @Test
+    void start() {
+        Game game = new Game();
+        try{
+            Field field = game.getClass().getDeclaredField("gameState");
+            field.setAccessible(true);
+            int value = (int)field.get(game);
+            assertEquals(value, 0); // gameState = START_GAME_STATE 확인
+            game.buttonPressed(); // gmae start
+            value = (int)field.get(game);
+            assertEquals(value, 1); // gameState = GAME_PLAYING_STATE
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e. printStackTrace();
+        }
+    }
 }
