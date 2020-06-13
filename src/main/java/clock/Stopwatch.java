f2a0485a96a763744e15c3a62dea867a716b1809
BoxBy <lute7071@gmail.com>
2020-06-13 16:51:53 +0900

    Stopwatch Split Bug Fix

--- src/main/java/clock/Stopwatch.java
+++ src/main/java/clock/Stopwatch.java
@@ -17,6 +17,7 @@ public class Stopwatch implements Mode {
     public Stopwatch() {
         this.stopwTime = Calendar.getInstance();
         stopwTime.clear();
+        splitstopwTime = Calendar.getInstance();
 
         isPaused = true;
         isSplit = false;
@@ -25,34 +26,34 @@ public class Stopwatch implements Mode {
 
     public String[] requestStopwTime() {
         String[] stopwBufferArr = new String[4];
-        if(this.stopwTime.get(Calendar.HOUR_OF_DAY)==0) {
+        Calendar tempStopwTime = (Calendar) stopwTime.clone();
 
-            Calendar tempStopwTime = (Calendar) stopwTime.clone();
+        StringBuffer nameBuffer = new StringBuffer(); // stopwatch
+        StringBuffer stopwBuffer = new StringBuffer(); //  minute:sec
+        StringBuffer csecBuffer = new StringBuffer(); //:csec
 
-            StringBuffer nameBuffer = new StringBuffer(); // stopwatch
-            StringBuffer stopwBuffer = new StringBuffer(); //  minute:sec
-            StringBuffer csecBuffer = new StringBuffer(); //:csec
+        nameBuffer.append("Stopwatch");
+        stopwBufferArr[0] = nameBuffer.toString();
 
-            nameBuffer.append("Stopwatch");
-            stopwBufferArr[0] = nameBuffer.toString();
+        stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE) < 10 ? "0" : "");
+        stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE));
+        stopwBuffer.append(":");
+        stopwBuffer.append(tempStopwTime.get(Calendar.SECOND) < 10 ? "0" : "");
+        stopwBuffer.append(tempStopwTime.get(Calendar.SECOND));
 
-            stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE) < 10 ? "0" : "");
-            stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE));
-            stopwBuffer.append(":");
-            stopwBuffer.append(tempStopwTime.get(Calendar.SECOND) < 10 ? "0" : "");
-            stopwBuffer.append(tempStopwTime.get(Calendar.SECOND));
+        stopwBufferArr[1] = stopwBuffer.toString();
 
-            stopwBufferArr[1] = stopwBuffer.toString();
+        csecBuffer.append(":");
+        csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) < 100 ? "0" : "");
+        csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) / 10);
 
-            csecBuffer.append(":");
-            csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) < 100 ? "0" : "");
-            csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) / 10);
+        stopwBufferArr[2] = csecBuffer.toString();
 
-            stopwBufferArr[2] = csecBuffer.toString();
+        stopwBufferArr[3] = "X";
 
-            stopwBufferArr[3] = "X";
-
-        }
+        if(isSplit)
+            stopwBufferArr[0] = requestSplit();
+//        System.out.println(stopwBufferArr[0] + "\n" + stopwBufferArr[1] + "\n" + stopwBufferArr[2] + "\n" + stopwBufferArr[3]);
         return stopwBufferArr;
     }
 
@@ -64,8 +65,10 @@ public class Stopwatch implements Mode {
 
     public void resetStopw() {
         // TODO implement here
-        if(this.isPaused == true)
+        if(this.isPaused == true) {
             this.stopwTime.clear();
+            isSplit = false;
+        }
     }
 
     public void increaseStopw() {
@@ -81,43 +84,37 @@ public class Stopwatch implements Mode {
 
     }
 
-    public String[] requestSplit() {
-        String[] splitBufferArr = new String[4];
-        this.isSplit = true;
+    public String requestSplit() {
 
-        if(this.isPaused == false && this.isSplit == true) {
+//        splitstopwTime.set(Calendar.MINUTE, stopwTime.get(Calendar.MINUTE));
+//        splitstopwTime.set(Calendar.SECOND, stopwTime.get(Calendar.SECOND));
+//        splitstopwTime.set(Calendar.MILLISECOND, stopwTime.get(Calendar.MILLISECOND));
+//
+//        Calendar tempStopwTime = (Calendar) stopwTime.clone();
+//
+//        StringBuffer nameBuffer = new StringBuffer();
+//        StringBuffer stopwBuffer = new StringBuffer(); //  minute:sec
+//        StringBuffer csecBuffer = new StringBuffer(); //:csec
+//
+//        nameBuffer.append("Stopwatch(S)");
+//        splitBufferArr[0] = nameBuffer.toString();
+//
+//
+//        stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE) < 10 ? "0" : "");
+//        stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE));
+//        stopwBuffer.append(":");
+//        stopwBuffer.append(tempStopwTime.get(Calendar.SECOND) < 10 ? "0" : "");
+//        stopwBuffer.append(tempStopwTime.get(Calendar.SECOND));
+//
+//        splitBufferArr[1] = stopwBuffer.toString();
+//
+//        csecBuffer.append(":");
+//        csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) < 100 ? "0" : "");
+//        csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) / 10);
+//
+//        splitBufferArr[2] = csecBuffer.toString();
 
-            splitstopwTime.set(Calendar.MINUTE, stopwTime.get(Calendar.MINUTE));
-            splitstopwTime.set(Calendar.SECOND, stopwTime.get(Calendar.SECOND));
-            splitstopwTime.set(Calendar.MILLISECOND, stopwTime.get(Calendar.MILLISECOND));
-
-            Calendar tempStopwTime = (Calendar) splitstopwTime.clone();
-
-            StringBuffer nameBuffer = new StringBuffer();
-            StringBuffer stopwBuffer = new StringBuffer(); //  minute:sec
-            StringBuffer csecBuffer = new StringBuffer(); //:csec
-
-            nameBuffer.append("Split");
-            splitBufferArr[0] = nameBuffer.toString();
-
-
-            stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE) < 10 ? "0" : "");
-            stopwBuffer.append(tempStopwTime.get(Calendar.MINUTE));
-            stopwBuffer.append(":");
-            stopwBuffer.append(tempStopwTime.get(Calendar.SECOND) < 10 ? "0" : "");
-            stopwBuffer.append(tempStopwTime.get(Calendar.SECOND));
-
-            splitBufferArr[1] = stopwBuffer.toString();
-
-            csecBuffer.append(":");
-            csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) < 100 ? "0" : "");
-            csecBuffer.append(tempStopwTime.get(Calendar.MILLISECOND) / 10);
-
-            splitBufferArr[2] = csecBuffer.toString();
-
-        }
-        splitBufferArr[3] = "X";
-        return splitBufferArr;
+        return String.format("%02d:%02d:%02d(S)", splitstopwTime.get(Calendar.MINUTE), splitstopwTime.get(Calendar.SECOND), splitstopwTime.get(Calendar.MILLISECOND) / 10);
     }
 
 
@@ -133,8 +130,10 @@ public class Stopwatch implements Mode {
     public void WPressed(boolean Longpress) {
         if(isPaused == true)
             resetStopw();
-        else
-            requestSplit();
+        else {
+            isSplit = true;
+            splitstopwTime = (Calendar)stopwTime.clone();
+        }
     }
     @Override
     public void SPressed(boolean Longpress) { // D
