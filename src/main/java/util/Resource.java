e004640e91ed6d014ce6cf3a1658540415806a56
BoxBy <lute7071@gmail.com>
2020-06-20 17:10:27 +0900

    Resource constructor FIX

--- src/main/java/util/Resource.java
+++ src/main/java/util/Resource.java
@@ -1,7 +1,5 @@
 package util;
 
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -12,9 +10,10 @@ import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 
 public class Resource {
-    private static final Logger logger = Logger.getLogger(Resource.class.getName());
+    private static final Logger LOGGER = Logger.getLogger(Resource.class.getName());
 
-    private Resource(){
+    public Resource(){
+        LOGGER.log(Level.WARNING, "WRONG ACCESS");
     }
 
 
@@ -24,7 +23,7 @@ public class Resource {
             URL imageURL = Thread.currentThread().getContextClassLoader().getResource(path);
             img = ImageIO.read(imageURL);
         } catch(IOException e){
-            logger.log(Level.WARNING, "Resource Thread Interrupted", e);
+            LOGGER.log(Level.WARNING, "Resource Thread Interrupted", e);
         }
         return img;
     }
@@ -35,7 +34,7 @@ public class Resource {
           font = Font.createFont(Font.TRUETYPE_FONT, Thread.currentThread().getContextClassLoader().getResourceAsStream("resources/scoreboard.ttf")).deriveFont(Font.PLAIN, size);
 
         }catch(IOException | FontFormatException e){
-            logger.log(Level.WARNING, "Font Thread Interrupted", e);
+            LOGGER.log(Level.WARNING, "Font Thread Interrupted", e);
         }
         return font;
     }
